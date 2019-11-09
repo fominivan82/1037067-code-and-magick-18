ec03dfe217d3f834551718d3320e5c8ac8cb1ac9
@fominivan82 <infomin@yndex.ru>
2019-11-09 23:25:14 +0300

    внес исправления

--- js/backend.js
+++ js/backend.js
@@ -2,16 +2,31 @@
 
 (function () {
 
-  window.load = function (onSuccess, onError) {
-    var URL = 'https://js.dump.academy/code-and-magick/data';
+  var SUCCESS = 200;
+  var SEC = 10000; // 10s
+  var loadURL = 'https://js.dump.academy/code-and-magick/data';
+  var saveURL = 'https://js.dump.academy/code-and-magick';
+  var loadMetod = 'GET';
+  var saveMetod = 'POST';
+
+  window.loadAndSave = function (onSuccess, onError, URL, metod, data) {
+
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      switch (true) {
+        case ((xhr.status === SUCCESS) && (metod === 'POST')):
+          window.closePopup();
+          onSuccess(xhr.response);
+          break;
+
+        case (xhr.status === SUCCESS):
+          onSuccess(xhr.response);
+          break;
+
+        default:
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
       }
     });
     xhr.addEventListener('error', function () {
@@ -21,43 +36,17 @@
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
 
-    xhr.timeout = 10000; // 10s
+    xhr.timeout = SEC;
 
-    xhr.open('GET', URL);
-    xhr.send();
-  };
-
-  window.save = function (data, onSuccess, onError) {
-
-    var URL = 'https://js.dump.academy/code-and-magick';
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        window.closePopup();
-        onSuccess(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = 10000; // 10s
-
-    xhr.open('POST', URL);
+    xhr.open(metod, URL);
     xhr.send(data);
   };
-  window.load(window.setup.successHandler, window.setup.errorHandler);
+
+  window.loadAndSave(window.setup.successHandler, window.setup.errorHandler, loadURL, loadMetod);
 
   window.util.setupForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
     var formData = new FormData(window.util.setupForm);
-    window.save(formData, window.setup.successHandler, window.setup.errorHandler);
+    window.loadAndSave(window.setup.successHandler, window.setup.errorHandler, saveURL, saveMetod, formData);
   });
 })();
